41106ae45ab42f3f214c823f5e379a3b70d26eac
Nehakandelkar <120322840+Nehakandelkar@users.noreply.github.com>
2026-04-24 14:30:33 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> st = new Stack();
+
+        for(int i=0; i<n; i++){
+            char ch = s.charAt(i);
+
+            //if ch is opening brace
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(ch == ')'){
+                    if(!st.isEmpty() && st.peek() == '('){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                if(ch == ']'){
+                    if(!st.isEmpty() && st.peek() == '['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                if(ch == '}'){
+                    if(!st.isEmpty() && st.peek() == '{'){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+
+        }
+
+        if(!st.isEmpty()) return false;
+
+
+
+        return true;
+
+
+    }
+}
